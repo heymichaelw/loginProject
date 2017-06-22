df07e01f4146954ff63979f91b50410c85b4fbee
Michael <heymichaelw@gmail.com>
2017-06-22 15:49:28 -0400

    commit

--- app.js
+++ app.js
@@ -28,13 +28,59 @@ app.use(session({
   saveUninitialized : true
 }));
 
+app.use(function(req, res, next){
+  var pathname = parseurl(req).pathname;
 
+  if (!req.session.user && pathname != '/login') {
+    let qs = pathname != '/login' ? "?next=" + pathname : '';
+    res.redirect('/login' + qs);
+  } else {
+    next();
+  }
+});
 
-
-
+app.use(function(req, res, next){
+  var views = req.session.views;
+  if (!views) {
+    views = req.session.views = {};
+  }
+  views = (views || 0) + 1;
+  next();
+});
 
 app.get('/login', function(req, res){
-  res.render('login', {});
+  var context = {
+    next : req.query.next
+  };
+  res.render('login', context);
+});
+
+app.get('/', function(req, res){
+  var context = {
+    'username' : req.session.user.username,
+    'views' : req.session.views
+  };
+  res.render('index', context);
+});
+
+app.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  var nextPage = req.body.next || '/';
+  var person = users.find(function(user){
+    return user.username == username;
+  });
+  if (person && person.password == password) {
+    req.session.user = person;
+  } else if (req.session.user) {
+    delete req.session.user;
+  }
+
+  if (req.session.user) {
+    res.redirect(nextPage);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.listen(3000);
